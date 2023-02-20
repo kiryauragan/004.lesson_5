2d493c4a2acc1bb834aec97bf8b4b0f7b51d7ee2
Kyrylo <kirilltaranskyi@gmail.com>
2023-02-20 09:10:56 +0200

    script.js updated

--- script.js
+++ script.js
@@ -1,29 +1,24 @@
-let operation = prompt("Choose between add, sub, multi, div", 'add');
+let question = prompt("Choose between add, sub, multi, div", "add");
 let num1 = Number(prompt("Enter first number:"));
 let num2 = Number(prompt("Enter second number:"));
 
 let result;
+let operation;
 
-switch (operation) {
-  case "add":
-    result = num1 + num2;
-    break;
-
-  case "sub":
-    result = num1 - num2;
-    break;
-
-  case "multi":
-    result = num1 * num2;
-    break;
-
-  case "div":
-    result = num1 / num2;
-    break;
-
-    default:
-        alert("Not funny, enter some numbers!!!");
-        break;
+if (question === "add") {
+  result = num1 + num2;
+  operation = "+";
+} else if (question === "sub") {
+  result = num1 - num2;
+  operation = "-";
+} else if (question === "multi") {
+  result = num1 * num2;
+  operation = "*";
+} else if (question === "div") {
+  result = num1 / num2;
+  operation = "/";
+} else {
+  alert("Check your numbers and try again!");
 }
 
 alert(`${num1} ${operation} ${num2} = ${result}`);
